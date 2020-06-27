3f6444af24844e8caf90602a7f88a391d888ef56
SandeshVakale <sandyrv.vakale@gmail.com>
2020-06-27 17:36:14 +0200

    summary redux saga aded

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
-import { Images } from '../Themes'
+import { connect } from 'react-redux'
+import { Colors, Images } from '../Themes'
+import SummaryActions from '../Redux/SummaryRedux'
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
-export default class LaunchScreen extends Component {
+class LaunchScreen extends Component {
+  componentDidMount () {
+    const { summaryRequest } = this.props
+    summaryRequest()
+  }
+
   render () {
+    console.log('props', this.props)
     return (
       <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
+        <View style={[styles.backgroundImage, {backgroundColor: Colors.ember}]} />
         <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
-
           <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
+            <Text style={[styles.sectionText, { fontSize: 36 }]}>
+              Covid-19
             </Text>
           </View>
-
         </ScrollView>
       </View>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    summary: state.summary
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    summaryRequest: () => dispatch(SummaryActions.summaryRequest())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LaunchScreen)
